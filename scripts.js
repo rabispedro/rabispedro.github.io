df74df3d0c7d32f1c249ac97ae0d69928883be74
Pedro Rabis <rabis.pedro@gmail.com>
2025-06-16 16:11:43 -0300

    feat: adicionado curriculo, animacoes accordion e email

--- scripts.js
+++ scripts.js
@@ -7,3 +7,17 @@ window.onload = () => {
 		"/____/\\___/_/ /\\__,_/  /_.___/\\___/_/ /_/ /_/      |___/_/_/ /_/\\__,_/\\____(_|_|_)   \n",
 		"         /___/                                                                       \n")
 }
+
+
+function toggleAccordion(element) {
+	if (!element) {
+		return
+	}
+
+	if(!element.classList.contains("active-acorddion")) {
+		element.classList.add("active-acorddion")
+		return
+	}
+
+	element.classList.remove("active-acorddion")
+}
